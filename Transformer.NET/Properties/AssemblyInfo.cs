68cf39e410d28d9cbdc3bf10c7d4f8f2e5035c51
Geoffrey McGill <geoff@ext.net>
2012-11-18 22:03:03 -0700

    Update to v2.1.0
    Signed-off-by: Geoffrey McGill <geoff@ext.net>

--- Transformer.NET/Properties/AssemblyInfo.cs
+++ Transformer.NET/Properties/AssemblyInfo.cs
@@ -1,17 +1,20 @@
 ﻿using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Security;
 
-[assembly: AssemblyTitle("Transformer.NET")]
-[assembly: AssemblyDescription("A .NET template parsing and transformation library. ")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Ext.NET, Inc.")]
+[assembly: AssemblyCopyright("Copyright © 2006-2012 Ext.NET, Inc.")]
+
+[assembly: AssemblyVersion("2.1.0")]
+[assembly: AssemblyTitle("Transformer.NET")]
 [assembly: AssemblyProduct("Transformer.NET")]
-[assembly: AssemblyCopyright("Copyright © 2006-2011 Ext.NET, Inc.")]
+[assembly: AssemblyDescription("A .NET template parsing and transformation library. ")]
+
+[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: CLSCompliantAttribute(true)]
 [assembly: ComVisible(false)]
 [assembly: Guid("11ca7159-f444-417a-8ce9-07d0f8a0009b")]
-[assembly: AssemblyVersion("2.0.1.*")]
-[assembly: AssemblyFileVersion("2.0.1.*")]
+[assembly: AllowPartiallyTrustedCallers]
